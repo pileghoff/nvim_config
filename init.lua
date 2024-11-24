da4a8f32f5ee29265390214cf52bd0a9ebf21ae4
Pil Eghoff <pil.eghoff@gmail.com>
2024-11-24 19:57:13 +0100

    Remove noice until further notice

--- init.lua
+++ init.lua
@@ -303,20 +303,20 @@ lsp.zls.setup(capabilities)
 require("flash").setup()
 
 -- Noice
-require("noice").setup({
-	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-		override = {
-			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-			["vim.lsp.util.stylize_markdown"] = true,
-			["cmp.entry.get_documentation"] = true, -- requires hr shush/nvim-cmp
-		},
-	},
-	-- you can enable a preset for easier configuration
-	presets = {
-		bottom_search = true, -- use a classic bottom cmdline for search
-		inc_rename = true, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = true, -- add a border to hover docs and signature help
-		long_message_to_split = true, -- long messages will be sent to a split
-	},
-})
+--require("noice").setup({
+--	lsp = {
+--		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--		override = {
+--			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--			["vim.lsp.util.stylize_markdown"] = true,
+--			["cmp.entry.get_documentation"] = true, -- requires hr shush/nvim-cmp
+--		},
+--	},
+--	-- you can enable a preset for easier configuration
+--	presets = {
+--		bottom_search = true, -- use a classic bottom cmdline for search
+--		inc_rename = true, -- enables an input dialog for inc-rename.nvim
+--		lsp_doc_border = true, -- add a border to hover docs and signature help
+--		long_message_to_split = true, -- long messages will be sent to a split
+--	},
+--})
