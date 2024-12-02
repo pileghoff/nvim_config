824ef77f90f14b61ea1dc85eb5d32669cca17170
Pil Eghoff <pil.eghoff@gmail.com>
2024-12-02 19:07:42 +0100

    Noice is less bad

--- init.lua
+++ init.lua
@@ -322,19 +322,21 @@ require("flash").setup()
 -- Noice
 require("noice").setup({
 	lsp = {
-		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true, -- requires hr shush/nvim-cmp
 		},
+		documentation = {
+			view = "hover",
+		},
 	},
+
 	-- you can enable a preset for easier configuration
 	presets = {
 		bottom_search = true, -- use a classic bottom cmdline for search
 		inc_rename = true, -- enables an input dialog for inc-rename.nvim
 		lsp_doc_border = true, -- add a border to hover docs and signature help
-		long_message_to_split = true, -- long messages will be sent to a split
 	},
 	routes = {
 		{
@@ -352,10 +354,6 @@ require("noice").setup({
 				error = false,
 			},
 			opts = { skip = true },
-		},
-		{
-			view = "cmdline",
-			filter = { min_length = 60 },
 		},
 	},
 })
